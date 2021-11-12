1a3e487eda1df195940dda34ccee3fedd6f4344a
prowes <34424042+prowes@users.noreply.github.com>
2021-11-12 19:54:59 +0200

    Add files via upload

---
+++ max_sum.py
@@ -0,0 +1,17 @@
+import sys
+def find_sum(initial_number):
+    consists = []
+    initial_number = int(initial_number)
+    i = 1
+    while i < initial_number + 1:
+        if initial_number - i >= i + 1:
+            initial_number -= i
+            consists.append(i)
+        i += 1
+    consists.append(initial_number)
+    print(len(consists))
+    for j in range(0, len(consists)):
+        print(consists[j], end=" ")
+
+if __name__ == '__main__':
+    find_sum(sys.stdin.read())
